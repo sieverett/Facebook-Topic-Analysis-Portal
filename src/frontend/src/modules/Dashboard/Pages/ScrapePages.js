9748cf7033a80d4bcdbdf82d43ec2655176935f4
Hugh Bellamy <hughbellars@gmail.com>
2017-05-02 14:16:45 +0700

    Move to nuget package instead of git submodule

--- src/frontend/src/modules/Dashboard/Pages/ScrapePages.js
+++ src/frontend/src/modules/Dashboard/Pages/ScrapePages.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { getPageScrapes, exportPages, getPages, scrapePages } from '../Common/Data/Actions';
-import Panel from '../Common/Panel';
+import Panel from '../Components/Common/Panel';
 import ExportPages from '../Components/ScrapePages/ExportPages';
 import PageScrapeList from '../Components/ScrapePages/PageScrapeList';
 import ScrapePageForm from '../Components/ScrapePages/ScrapePageForm';
