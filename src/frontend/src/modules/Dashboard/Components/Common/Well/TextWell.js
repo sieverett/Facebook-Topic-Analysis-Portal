436ea82dde7c866c2e60a3f47bb89135dcd053cb
Hugh Bellamy <hughbellars@gmail.com>
2017-05-02 15:01:00 +0700

    Add click support to TextWell

--- src/frontend/src/modules/Dashboard/Components/Common/Well/TextWell.js
+++ src/frontend/src/modules/Dashboard/Components/Common/Well/TextWell.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 class TextWell extends Component {
+  header = () => {
+    const header =  <h2>{this.props.header} <small className="text-muted">{this.props.subheader}</small></h2>;
+    if (this.props.onClick) {
+      return <a href="#" onClick={e => { e.preventDefault(); this.props.onClick(); }}>{header}</a>
+    }
+
+    return header;
+  }
+
   render() {
+    const className = (this.props.onClick ? 'selectable-well ' : '') + 'well';
+
     return (
       <div className={this.props.className}>
-        <div className="well">
-          <h2>{this.props.header} <small className="text-muted">{this.props.subheader}</small></h2>
+        <div className={className}>
+          {this.header()}
         </div>
       </div>
     );
